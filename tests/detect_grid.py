2c4500c561727bbd5b84aebbcb5bf0771828c204
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-05-07 07:42:37 -0400

    detect_grid.py: Identifies the boundary lines

--- tests/detect_grid.py
+++ tests/detect_grid.py
@@ -5,6 +5,8 @@ import cv2
 import os
 import numpy as np
 import math
+import statistics
+import sys
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s \t%(message)s')
 
@@ -61,9 +63,9 @@ def main(
     rho_res = 1
     theta_res = 2 * np.pi/360
     threshold = 200
-    print (thresholded_sobelxy_mask)
+    #print (thresholded_sobelxy_mask)
     lines = cv2.HoughLines(thresholded_sobelxy_mask, rho_res, theta_res, threshold)
-    print(f"lines:\n{lines}")
+    #print(f"lines:\n{lines}")
     hough_lines_img = copy.deepcopy(original_img)
     vertical_lines = []
     horizontal_lines = []
@@ -78,21 +80,32 @@ def main(
         if abs(theta - np.pi/2) < 0.1 or abs(theta - 3 * np.pi/2) < 0.1:
             line_is_horizontal = True
             horizontal_lines.append(line)
-        a = math.cos(theta)
-        b = math.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = round(x0 - 1000 * b)
-        y1 = round(y0 + 1000 * a)
-        x2 = round(x0 + 1000 * b)
-        y2 = round(y0 - 1000 * a)
         color = (0, 0, 0)
         if line_is_vertical:
             color = (0, 255, 0)
         elif line_is_horizontal:
             color = (255, 0, 0)
-        cv2.line(hough_lines_img, (x1, y1), (x2, y2), color=color, thickness=1)
+        DrawLine(hough_lines_img, line[0], color)
+    north_line, south_line = BoundaryHorizontalLines(horizontal_lines)
+    DrawLine(hough_lines_img, north_line, (255, 0, 255))
+    DrawLine(hough_lines_img, south_line, (255, 0, 255))
+    west_line, east_line = BoundaryVerticalLines(vertical_lines)
+    DrawLine(hough_lines_img, west_line, (0, 255, 255))
+    DrawLine(hough_lines_img, east_line, (0, 255, 255))
     cv2.imwrite(os.path.join(outputDirectory, "main_houghLines.png"), hough_lines_img)
+
+def DrawLine(image, rho_theta, color):
+    rho = rho_theta[0]
+    theta = rho_theta[1]
+    a = math.cos(theta)
+    b = math.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = round(x0 - 1000 * b)
+    y1 = round(y0 + 1000 * a)
+    x2 = round(x0 + 1000 * b)
+    y2 = round(y0 - 1000 * a)
+    cv2.line(image, (x1, y1), (x2, y2), color=color, thickness=1)
 
 def CrossKernel(width=3, length_ratio=15, output_directory=None):
     kernel_sizeHW = (length_ratio * width, length_ratio * width)
@@ -104,6 +117,52 @@ def CrossKernel(width=3, length_ratio=15, output_directory=None):
     if output_directory is not None:
         cv2.imwrite(os.path.join(output_directory, "CrossKernel_crossKernel.png"), cross_kernel)
     return cross_kernel
+
+def BoundaryHorizontalLines(horizontal_lines):
+    rho_thetas = []
+    for line in horizontal_lines:
+        rho = line[0][0]
+        theta = line[0][1]
+        if abs(theta - 1.5 * math.pi) < 0.1:
+            theta -= math.pi
+            rho = -rho
+        rho_thetas.append((rho, theta))
+    # Sort by rho. Cf. https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
+    rho_thetas.sort(key=lambda tup: tup[0])
+    # North line: median of 1st quartile
+    north_thetas = [theta for (rho, theta) in rho_thetas[0: len(rho_thetas)//4]]
+    north_rhos = [rho for (rho, theta) in rho_thetas[0: len(rho_thetas)//4]]
+    north_theta = statistics.median(north_thetas)
+    north_rho = statistics.median(north_rhos)
+    # South line: median of 4th quartile
+    south_thetas = [theta for (rho, theta) in rho_thetas[round(0.75 * len(rho_thetas)):]]
+    south_rhos = [rho for (rho, theta) in rho_thetas[round(0.75 * len(rho_thetas)):]]
+    south_theta = statistics.median(south_thetas)
+    south_rho = statistics.median(south_rhos)
+    return (north_rho, north_theta), (south_rho, south_theta)
+
+def BoundaryVerticalLines(vertical_lines):
+    rho_thetas = []
+    for line in vertical_lines:
+        rho = line[0][0]
+        theta = line[0][1]
+        if abs(theta - math.pi) < 0.1:
+            theta -= math.pi
+            rho = -rho
+        rho_thetas.append((rho, theta))
+    # Sort by rho. Cf. https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
+    rho_thetas.sort(key=lambda tup: tup[0])
+    # West line: median of 1st quartile
+    west_thetas = [theta for (rho, theta) in rho_thetas[0: len(rho_thetas)//4]]
+    west_rhos = [rho for (rho, theta) in rho_thetas[0: len(rho_thetas)//4]]
+    west_theta = statistics.median(west_thetas)
+    west_rho = statistics.median(west_rhos)
+    # East line: median of 4th quartile
+    east_thetas = [theta for (rho, theta) in rho_thetas[round(0.75 * len(rho_thetas)):]]
+    east_rhos = [rho for (rho, theta) in rho_thetas[round(0.75 * len(rho_thetas)):]]
+    east_theta = statistics.median(east_thetas)
+    east_rho = statistics.median(east_rhos)
+    return (west_rho, west_theta), (east_rho, east_theta)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
